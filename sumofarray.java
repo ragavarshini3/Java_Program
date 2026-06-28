9abe56bc014c1889e2c7a0c1a707940a98100283
RAGAVARSHINI ALAGARSAMY <152855194+ragavarshini3@users.noreply.github.com>
2026-06-28 19:55:45 +0530

    Add files via upload

---
+++ sumofarray.java
@@ -0,0 +1,16 @@
+package Arrays;
+import java.util.Scanner;
+public class sumofarray {
+	public static void main(String[]args) {
+		Scanner sc= new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int []a=new int[n];
+	    int sum=0;
+	    for(int i=0; i<n ; i++){
+	        a[i]=sc.nextInt();
+	        sum+=a[i];
+	    }
+	    System.out.println("Sum = " + sum);
+	  }
+
+	}
